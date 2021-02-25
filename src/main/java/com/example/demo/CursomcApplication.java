a3a1c6ef8bab6b41c3e46694e6a70665efac561e
brunolbeltrao <brunolbeltrao@gmail.com>
2021-02-25 18:23:20 -0300

    criando operacao de instanciacao

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.domain.Categoria;
+import com.example.demo.repository.CategoriaRepository;
+import java.util.Arrays;
+
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+	
+	
+	
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Geografia");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
+	
 }
+
---
+++ src/main/java/com/example/demo/services/CategoriaServices.java
@@ -0,0 +1,21 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repository.CategoriaRepository;
+
+@Service
+public class CategoriaServices {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
